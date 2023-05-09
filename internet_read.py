0e87dc064b8f91bafcdd142259b74b6590170ad2
BMugo84 <mugo.benson.kbm@gmail.com>
2023-05-09 23:45:22 +0300

    chatgpt answer

--- internet_read.py
+++ internet_read.py
@@ -1,22 +1,34 @@
-import urllib.request  # import the urllib.request module to make HTTP requests
-import json  # import the json module to work with JSON data
+import urllib.request
+import json
 
-# create a new HTTP request to the specified URL
-my_request = urllib.request.urlopen("https://studies.cs.helsinki.fi/stats-mock/api/courses")
+def retrieve_all():
+    """
+    Retrieves the data of all courses that are currently active.
 
-# read the data returned by the HTTP request and store it in a variable
-data = my_request.read()
+    Returns:
+        A list of tuples, where each tuple contains the name of the course, name, year,
+        and the sum of the values listed in exercises.
+    """
+    # create a new HTTP request to the specified URL
+    my_request = urllib.request.urlopen("https://studies.cs.helsinki.fi/stats-mock/api/courses")
 
-# print the raw data returned by the HTTP request
-# print(data)
+    # read the data returned by the HTTP request and store it in a variable
+    data = my_request.read()
 
-# parse the JSON data in the 'data' variable into a Python list of dictionaries
-courses = json.loads(data)
+    # parse the JSON data in the 'data' variable into a Python list of dictionaries
+    courses = json.loads(data)
 
-# print the parsed data
-# print(courses)
+    active = []
+    # iterate over the list of courses
+    for item in courses:
+        # check if the course is enabled
+        if item["enabled"]:
+            # calculate the sum of the exercises
+            sum_exercises = sum(item["exercises"])
+            # create a tuple with the required fields and append it to the 'active' list
+            tup = (item["fullName"], item["name"], item["year"], sum_exercises)
+            active.append(tup)
+    return active
 
-for item in courses:
-    if item["enabled"] == True:
-        sum_exercises = sum(item["exercises"])
-        print(f'({item["fullName"]}, {item["name"]}, {item["year"]}, {sum_exercises})')
+# print the list of active courses
+print(retrieve_all())
