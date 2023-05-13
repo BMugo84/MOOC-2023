8b4e00238354461bb8192d588b81ef0515231092
BMugo84 <mugo.benson.kbm@gmail.com>
2023-05-14 01:13:01 +0300

    done with str function in classes

--- Week 8/methods.py
+++
@@ -1,9 +0,0 @@
-class BankAccount:
-
-    def __init__(self, account_number: str, owner: str, balance: float, annual_interest: float) :
-        self.account_number = account_number
-        self.owner = owner
-        self.balance = balance
-        self.annual_interest = annual_interest
-
-peters_account = BankAccount("12345-678", "Peter Python", 1500.0, 0.015)
