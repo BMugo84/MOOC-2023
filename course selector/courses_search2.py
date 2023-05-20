c23242ae8d56d8fe4d647295f564fa0c73fc0722
BMugo84 <mugo.benson.kbm@gmail.com>
2023-05-20 19:58:11 +0300

    works perfectly!

--- course selector/courses_search2.py
+++ course selector/courses_search2.py
@@ -4,7 +4,7 @@ import constants as keys
 import pandas as pd
 
 # conversation handler 
-UNIVERSITY, COURSE, CUTOFF = range(3)
+UNIVERSITY, COURSE = range(2)
 
 # keyboard buttons for universities
 universities = [
@@ -48,8 +48,8 @@ df = pd.read_excel('DEGREE COURSES.xlsx')
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     reply_keyboard = [[university] for university in universities]
     await update.message.reply_text(    # type: ignore
-        "Hello! I am Brainhost bot here to help you select courses!"
-        "Which course branch are you interested in?",
+        "Hello! I am the Brainhost bot, your course selection assistant!"
+        "Please select a university from the list below to explore the available courses:",
         reply_markup = ReplyKeyboardMarkup(
             reply_keyboard,
             one_time_keyboard=True,
@@ -64,45 +64,57 @@ async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 async def handle_university(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
+    """
+    This function handles the user's university selection and prompts for the course selection.
+    """
     # fetch text from start_command output
-    text = update.message.text# type: ignore
-    text = text.upper()# type: ignore
+    text: str = update.message.text.upper()
 
     # create another keyboardbutton for courses
     filtered_df = df.loc[df['INSTITUTION NAME'] == text]
     courses = filtered_df['PROGRAMME NAME'].tolist()
     reply_keyboard = ReplyKeyboardMarkup([[course] for course in courses], one_time_keyboard=True)
 
-    await update.message.reply_text(       # type: ignore
+    await update.message.reply_text( 
         "Please select the course needed",
         reply_markup = reply_keyboard
     )
 
-    context.user_data['course_response'] = update.message.text # type: ignore
+    context.user_data['course_response'] = update.message.text 
 
     return COURSE
 
 
 async def handle_course(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """
+    This function handles the user's course selection and displays the cutoff points for the selected course.
+    """
     text = update.message.text
-    # fetch text from handle_university output
-    start_response = context.user_data.get('university_response')# type: ignore
-    first_level_response = context.user_data.get('course_response')# type: ignore
+    start_response = context.user_data.get('university_response')  # Fetch text from handle_university output
+    first_level_response = context.user_data.get('course_response')  # Fetch text from course selection
 
-    # print out cutoff points for the selected course
-    filtered_df = df.loc[(df['PROGRAMME NAME'] == first_level_response.upper()) & (df['INSTITUTION NAME'] == start_response.upper())]# type: ignore
+    # Filter the dataframe to select the cutoff points for the selected course and institution
+    filtered_df = df.loc[(df['PROGRAMME NAME'] == text.upper()) & (df['INSTITUTION NAME'] == first_level_response.upper())]
     cutoffs = filtered_df[['2015 CUTOFF', '2016 CUTOFF', '2017 CUTOFF', '2018 CUTOFF', '2019 CUTOFF', '2020 CUTOFF', '2021 CUTOFF']]
+
+    cutoffs = cutoffs.to_dict(orient='list')
+    print(cutoffs)
+
+    output_lines = []
+    for year, cutoff_values in cutoffs.items():
+        line = f"{year}: {', '.join(map(str, cutoff_values))}"
+        output_lines.append(line)
+
     await update.message.reply_text(
-        f'{cutoffs}'
+        "\n".join(output_lines)
     )
 
-    return CUTOFF
 
 async def exit(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     """Cancels and ends the conversation."""
     user = update.message.from_user
     await update.message.reply_text(
-        "Bye! I hope we can talk again some day.", reply_markup=ReplyKeyboardRemove()
+        "Bye! I hope we can talk again some day."
     )
 
     return ConversationHandler.END
@@ -112,14 +124,6 @@ async def exit(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
 if __name__ == '__main__':
     app = Application.builder().token(keys.API_KEY).build()
 
-    # # add handler to handle user input 
-    # app.add_handler(CommandHandler('start', start_command))
-
-    # # add handler to handle university messages
-    # app.add_handler(MessageHandler(filters.TEXT & ~(filters.COMMAND), handle_university))
-
-    # # add handler to handle courses
-    # app.add_handler(MessageHandler(filters.TEXT & ~(filters.COMMAND), handle_course))
 
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler('start', start_command)],
